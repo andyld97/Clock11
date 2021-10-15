9bf00065d03572313cad71ab12f428ccd2f78ea6
Andreas Leopold <andreas@instazoom.de>
2021-10-15 15:40:03 +0200

    feat: Inital commit

---
+++ Windows11Clock/ClockWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows;
+
+namespace Windows11Clock
+{
+    /// <summary>
+    /// Interaction logic for ClockWindow.xaml
+    /// </summary>
+    public partial class ClockWindow : Window
+    {
+        public ClockWindow()
+        {
+            InitializeComponent();
+            RefreshClock(DateTime.Now);
+        }
+
+        public void RefreshClock(DateTime now)
+        {
+            TextClockTime.Text = now.ToString("t");
+            TextClockDate.Text = now.ToString("d");
+        }
+
+        public void BringToFront()
+        {
+            Topmost = false;
+            Topmost = true;
+            Activate();
+        }
+    }
+}
---
+++ Windows11Clock/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace Windows11Clock
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly DispatcherTimer clockTimer = new DispatcherTimer();
+        private readonly List<ClockWindow> clockWindows = new List<ClockWindow>();
+        
+        private const int LEFT_MARGIN = 60;
+        private const int TOP_MARGIN = 10;
+        private const int CLOCK_TIMER_REFRESH_INTERVAL = 100; // [ms.]
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitalizeClockWindows();
+   
+            // Initalize clock timer
+            clockTimer.Interval = TimeSpan.FromMilliseconds(CLOCK_TIMER_REFRESH_INTERVAL);
+            clockTimer.Tick += ClockTimer_Tick;
+            clockTimer.Start();
+
+            // Register hotkey
+            GlobalHotKey.RegisterHotKey("Ctrl + U", () => clockWindows.ForEach(w => w.BringToFront()));
+        }
+
+        private void InitalizeClockWindows()
+        {
+            clockWindows.Clear();
+
+            foreach (var screen in WpfScreen.AllScreens())
+            {
+                if (screen.IsPrimary)
+                    continue;
+
+                var area = screen.WorkingArea;
+                ClockWindow clockWindow = new ClockWindow
+                {
+                    Left = area.Left + area.Width - LEFT_MARGIN,
+                    Top = area.Top + area.Height + TOP_MARGIN,
+                };
+
+                clockWindows.Add(clockWindow);
+                clockWindow.Show();
+            }
+        }
+
+        private void ClockTimer_Tick(object? sender, EventArgs e)
+        {
+            DateTime now = DateTime.Now;
+            clockWindows.ForEach(w => w.RefreshClock(now));
+        }
+    }
+}
